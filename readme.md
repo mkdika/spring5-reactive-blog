f8d7d56f7f61999118d2986fd4e244a818640ddb
Maikel Chandika <mkdika@gmail.com>
2017-08-08 15:08:32 +0700

    add model class

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-## [Spring](https://spring.io/) 5 Reactive - Blog Scenario Example
+## [Spring](https://spring.io/) 5 Reactive - Blog Project Sample
 [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](/LICENSE)
 
 ### Framework & Libraries
--- src/main/java/com/mkdika/spring5reactiveblog/model/Comment.java
+++ src/main/java/com/mkdika/spring5reactiveblog/model/Comment.java
@@ -3,18 +3,18 @@ package com.mkdika.spring5reactiveblog.model;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.Date;
-import java.util.List;
 
 @Data
 @Document
-public class Post {
+public class Comment {
 
     @Id
     private String id;
-    private String title;
+    private String name;
+    private String email;
+    private Date commenttime;
     private String body;
-    private Date posttime;
-    private List<Comment> comments;
 
 }
